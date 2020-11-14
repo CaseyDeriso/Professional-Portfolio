f31acb2a2d2a4569c6094b098fee6f618f4b221f
Casey <caseyderiso@gmail.com>
2020-11-13 21:23:36 -0600

    added description text when hovering over example-text-containers

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+let exampleText = document.querySelectorAll(".example-text-container");
+
+for (let i=0; i < exampleText.length; i++) {
+exampleText[i].addEventListener("mouseenter", function() {
+    exampleText[i].querySelector(".hidden-text").style.display = "flex";
+});
+}
+for (let i=0; i < exampleText.length; i++) {
+    exampleText[i].addEventListener("mouseleave", function() {
+        exampleText[i].querySelector(".hidden-text").style.display = "none";
+    });
+    }
